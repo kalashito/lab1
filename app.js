5183d47bfa28429a28b83e7ffc0758a9b0ef69e8
kalashito <smil787898@gmail.com>
2021-04-13 14:23:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+﻿
+require('dotenv').config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+
+    res.send(process.env.HELLO);
+});
+
+app.get("/user", (req, res) => {
+
+    res.send({name: process.env.NAME, age: process.env.AGE});
+});
+
+app.listen(3000);0);"# 4course_labs"  app.test.js package.json package-lock.json server.js start.js sum.js sum.test.js node_modules re.env
---
+++ server.js
@@ -0,0 +1,11 @@
+//server.js
+const express = require('express');
+const app = express();
+
+app.use("/test", (req, res) => {
+    res.send({message: 'pass!'});
+    res.end();
+
+});
+
+module.exports = app;
